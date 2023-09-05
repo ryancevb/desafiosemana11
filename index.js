7113d4582f8a6efcef241a1cb673351320457446
ryancevb <141280376+ryancevb@users.noreply.github.com>
2023-09-05 05:08:52 -0300

    cambios

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-/*fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));*/
+//Metodo POST 
+document.getElementById("formulario").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let datosNombre = document.getElementById("nombre").value
+  let datosApellido = document.getElementById("apellido").value
+  let datosFecha = document.getElementById("fecha").value
 
-  let datosNombre= document.getElementById("nombre").value
-  let datosApellido= document.getElementById("apellido").value
-  let datosFecha= document.getElementById("fecha").value
+  let datosFormulario = {
+    nombre: datosNombre,
+    apellido: datosApellido,
+    fecha: datosFecha
+  };
 
-  fetch(' https://jsonplaceholder.typicode.com/users.')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-  
+  fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(datosFormulario)
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      console.error('Error:', error);
+    });
+});
